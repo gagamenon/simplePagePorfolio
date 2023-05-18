1040dca4d70efa5fe9af28a612313a5f2eccbbe0
Gab Parra <gabriel@gabitron.com>
2023-05-18 00:49:18 +0000

    add js  to resize map, minor styles fixes

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,16 @@
+// Resize map widht to fit content 
+const mapFrame = document.querySelector("#map-frame");
+const formContainer = document.querySelector("#form-container");
+const footerTwoColumns = document.querySelector("#footer-two-columns");
+
+function updateSize() {
+	if (window.innerWidth > 768) {
+		mapFrame.style.width = (footerTwoColumns.clientWidth)/2 + "px";
+		mapFrame.style.height = (footerTwoColumns.clientWidth)/2 + "px";	
+	} else {
+	mapFrame.style.width = formContainer.clientWidth + "px";
+	mapFrame.style.height = formContainer.clientWidth + "px";	}
+}
+updateSize();
+window.addEventListener("resize", updateSize);
 
